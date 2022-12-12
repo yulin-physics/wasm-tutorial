f1e9382912bd6f0cab90318e484bd5dc53d5899b
Yulin Chen <yulin@kaleidoscope.com>
2022-12-12 18:07:29 +0000

    update alert to counter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,91 @@
 use seed::{prelude::*, *};
 
+struct Model {
+    counter: i32,
+}
+
+enum Msg {
+    Increment,
+    Decrement,
+    Reset,
+}
+
+fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model { counter: 0 }
+}
+
+fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Increment => model.counter += 1,
+        Msg::Decrement => model.counter -= 1,
+        Msg::Reset => model.counter = 0,
+    }
+}
+
+fn view(model: &Model) -> Node<Msg> {
+    div![div![
+        style! {
+            St::Display => "flex",
+            St::Padding => px(50),
+            St::FontSize => px(20),
+        },
+        div![
+            style! {
+                St::Padding => px(20),
+                St::FontSize => px(20),
+            },
+            button![
+                "RESET",
+                style! {
+                    St::Padding => px(20),
+                    St::FontSize => px(20),
+                    St::BackgroundColor => "grey",
+                },
+                ev(Ev::Click, |_| Msg::Reset)
+            ]
+        ],
+        div![
+            style! {
+                St::Padding => px(20),
+                St::FontSize => px(20),
+            },
+            button![
+                "ADD",
+                style! {
+                    St::Padding => px(20),
+                    St::FontSize => px(20),
+                    St::BackgroundColor => "green",
+                },
+                ev(Ev::Click, |_| Msg::Increment)
+            ]
+        ],
+        div![
+            style! {
+                St::Padding => px(20),
+                St::FontSize => px(20),
+            },
+            button![
+                "SUBTRACT",
+                style! {
+                    St::Padding => px(20),
+                    St::FontSize => px(20),
+                    St::BackgroundColor => "red",
+                },
+                ev(Ev::Click, |_| Msg::Decrement)
+            ]
+        ],
+        div![
+            style! {
+                St::Padding => px(20),
+                St::FontSize => px(20),
+            },
+            "COUNTER: ",
+            model.counter,
+        ],
+    ]]
+}
+
 fn main() {
-    window().alert_with_message("Hello, World");
+    // MVC model
+    App::start("app", init, update, view);
 }
